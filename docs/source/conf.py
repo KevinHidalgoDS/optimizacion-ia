fc29b97cca55a0ad1ecce14e6921106f35ada765
kevin.hidalgo <xm_e_khidalgo@xm.com.co>
2026-09-04 10:56:43 -0500

    prueba sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,56 +1,105 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-import os
 import sys
+from pathlib import Path
 
-sys.path.insert(0, os.path.abspath('../../'))
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+# -- Configuración de Rutas (Pathlib) ----------------------------------------
+# Raíz del proyecto: dos niveles arriba de docs/source/
+ROOT_DIR = Path(__file__).resolve().parents[2]
+sys.path.insert(0, str(ROOT_DIR))
+
+# Evita problemas de recursión profunda en inspecciones complejas de autodoc
 sys.setrecursionlimit(5000)
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
+# -- Información del Proyecto ------------------------------------------------
 project = 'Optimización-ia'
 copyright = '2026, Kevin Hidalgo'
 author = 'Kevin Hidalgo'
 release = '1.0.0'
+version = '1.0'
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
+# -- Configuración General ---------------------------------------------------
 extensions = [
+    # Extracciones automáticas de docstrings
+    'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.napoleon',
-    'sphinx.ext.autodoc',
+    # Enlaces cruzados y utilidades
     'sphinx.ext.intersphinx',
-    'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
+    'sphinx.ext.ifconfig',
+    # Tema
     'sphinx_rtd_theme',
 ]
 
-# Generate autodoc stubs with summaries from code
+# Configuración de idioma y documento raíz
+language = 'es'
+root_doc = 'index'
+master_doc = 'index'  # Compatibilidad con versiones Sphinx < 4.0
 
+# Archivos y carpetas a ignorar en la compilación
+exclude_patterns = [
+    '_build',
+    'Thumbs.db',
+    '.DS_Store',
+    '**.ipynb_checkpoints',
+]
+
+templates_path = ['_templates']
+
+# -- Configuración de Autodoc & Autosummary ---------------------------------
 autosummary_generate = True
 autosummary_generate_overwrite = True
 
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
-exclude_patterns = []
-mathjax3_config = {
-  "tex": {
-    "inlineMath": [['\\(', '\\)']],
-    "displayMath": [["\\[", "\\]"]],
-  }
+# Mantiene el orden de definición original del código (no alfabético)
+autodoc_member_order = 'bysource'
+
+# Formato de type hints: 'signature', 'description' o 'none'
+autodoc_typehints = 'description'
+
+# Opciones por defecto para documentar miembros
+autodoc_default_options = {
+    'members': True,
+    'undoc-members': True,
+    'show-inheritance': True,
 }
 
-language = 'es'
+# (Opcional) Si en el futuro usas librerías pesadas o de nube difíciles de instalar en CI/CD:
+# autodoc_mock_imports = ["azure", "pyspark"]
 
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+# -- Configuración de Napoleon (Google / NumPy Docstrings) -------------------
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = True
+napoleon_include_private_with_doc = False
+napoleon_use_param = True
+napoleon_use_rtype = True
+napoleon_preprocess_types = True
 
-master_doc = 'index'
+# -- Configuración de Intersphinx --------------------------------------------
+# Permite vincular tipos de datos nativos a la documentación de Python oficial
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3', None),
+}
+
+# -- Configuración de MathJax (LaTeX) ----------------------------------------
+mathjax3_config = {
+    'tex': {
+        'inlineMath': [['\\(', '\\)']],
+        'displayMath': [['\\[', '\\]']],
+    }
+}
+
+# -- Opciones de Salida HTML -------------------------------------------------
 html_theme = 'sphinx_rtd_theme'  # alabaster, sphinx_rtd_theme, groundwork
 html_static_path = ['_static']
+
+# Opciones avanzadas del tema Read the Docs
+html_theme_options = {
+    'navigation_depth': 4,
+    'collapse_navigation': False,
+    'sticky_navigation': True,
+    'includehidden': True,
+    'titles_only': False,
+}
